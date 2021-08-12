cef06a88c16efca5dc6a53a7fad2a82bb7e7dd8f
Andrii Yukhymenko <andriy111224@gmail.com>
2021-08-12 22:52:34 +0300

    Написал рендер значений из инпутов

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,31 @@
-/**
-  * название функции
-  *
-  * @param {number} first - первое число
-  * @returns {number}
-  */
 "use strict";
+
+var figureBody = document.querySelector('.figure__body'),
+    borderInputs = document.querySelectorAll('.figure__border-input'),
+    figureResult = figureBody.querySelector('.figure__result'),
+    checkboxCss = document.querySelector('input#css'),
+    checkboxWebkit = document.querySelector('input#css'),
+    checkboxGecko = document.querySelector('input#css'); // console.log(checkboxCss.checked);
+
+figureBody.addEventListener('input', function (event) {
+  if (event.target.classList.contains('figure__border-input--top-left') || event.target.classList.contains('figure__border-input--top-right') || event.target.classList.contains('figure__border-input--bottom-left') || event.target.classList.contains('figure__border-input--bottom-right')) {
+    calculate();
+  }
+});
+
+function calculate() {
+  var position = event.target.id;
+  figureResult.innerText = '';
+  borderInputs.forEach(function (item, number) {
+    if (item.value !== '' && item.value !== null) {
+      // if()
+      // if (i){}
+      // else {}
+      console.log(item.value);
+      figureResult.innerHTML += 'border-' + position + '-radius: ' + item.value + 'px; <br>';
+    }
+  });
+}
+
+function renderBorders() {}
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,36 @@
-/**
-  * название функции
-  *
-  * @param {number} first - первое число
-  * @returns {number}
-  */
+let figureBody = document.querySelector('.figure__body'),
+  borderInputs = document.querySelectorAll('.figure__border-input'),
+  figureResult = figureBody.querySelector('.figure__result'),
+  checkboxCss = document.querySelector('input#css'),
+  checkboxWebkit = document.querySelector('input#css'),
+  checkboxGecko = document.querySelector('input#css');
+// console.log(checkboxCss.checked);
+
+figureBody.addEventListener('input', (event) => {
+  if (
+    event.target.classList.contains('figure__border-input--top-left') ||
+    event.target.classList.contains('figure__border-input--top-right') ||
+    event.target.classList.contains('figure__border-input--bottom-left') ||
+    event.target.classList.contains('figure__border-input--bottom-right')
+  ) {
+    calculate();
+  }
+});
+
+function calculate() {
+  let position = event.target.id;
+  figureResult.innerText = '';
+  borderInputs.forEach((item, number) => {
+    if (item.value !== '' && item.value !== null) {
+      // if()
+      // if (i){}
+      // else {}
+      console.log(item.value);
+      figureResult.innerHTML += 'border-' + position + '-radius: ' + item.value + 'px; <br>';
+    }
+  });
+}
+
+function renderBorders(){
+
+}
